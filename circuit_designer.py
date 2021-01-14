1c9132ea32823fb3233084cb14f5cc388da5efac
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2021-01-13 18:10:20 -0600

    Se agregan funciones para generar uns lista de resistencias y un diccionario, también otra para buscar resistencias por valor y así obtener su nombre

--- circuit_designer.py
+++ circuit_designer.py
@@ -3,17 +3,16 @@
 '''
 
 
-
 class Node:
-    
+
     def __init__(self, ident):
         self.id = ident
         self.volt = 0
         self.current = 0
-        
+
     def setVolt(self, volt):
         self.volt = volt
-        
+
     def setCurrent(self, cur):
         self.current = cur
 
@@ -22,10 +21,10 @@ class Node:
 
     def getCurrent(self):
         return self.current
-    
+
     def getId(self):
         return self.id
-    
+
 
 class Arc:
 
@@ -40,14 +39,16 @@ class Arc:
         elif component == "source":
             self.volts = value
         '''
+
     def getValue(self):
         return self.value
-        
+
+
 class Graph:
 
     def __init__(self):
         self.adMatrix = []
-        self.nodes = [] #Nodos 
+        self.nodes = []  # Nodos
         self.nodesCount = 0
 
     def checkNode(self, ident):
@@ -62,7 +63,7 @@ class Graph:
         for i in self.nodes:
             if i.getId() == ident:
                 return i
-        
+
     def addNode(self, ident):
         self.nodesCount += 1
         self.nodes += [Node(ident)]
@@ -75,21 +76,21 @@ class Graph:
                 for j in range(self.nodesCount):
                     tmp_aij += [None]
                 tmp += [tmp_aij]
-                
+
             trans = self.adMatrix
             for i in range(self.nodesCount - 1):
                 for j in range(self.nodesCount - 1):
                     tmp[i][j] = trans[i][j]
             self.adMatrix = tmp
-            
-    def addArc(self, id1, id2, component,value=0):
+
+    def addArc(self, id1, id2, component, value=0):
         arc = Arc(component, value)
         if self.checkNode(id1) and self.checkNode(id2):
             index1 = self.nodes.index(self.getById(id1))
             index2 = self.nodes.index(self.getById(id2))
             self.adMatrix[index1][index2] = arc
 
-    def deleteNode(self, ident): #Elimina los arcos asociados a ese nodo tanto de ida como de vuleta
+    def deleteNode(self, ident):  # Elimina los arcos asociados a ese nodo tanto de ida como de vuleta
         if self.checkNode(ident):
             tmp = []
             self.nodesCount -= 1
@@ -98,28 +99,43 @@ class Graph:
             self.adMatrix.pop(ind)
             for i in self.adMatrix:
                 i.pop(ind)
-            
-        
+
     def getNodes(self):
         return self.nodes
-    
+
     def printGraph(self):
         for i in self.adMatrix:
             print("[", end="")
             for j in i:
                 if j != None:
-                    print(j.component+":"+str(j.value)+" ", end="")
+                    print(j.component + ":" + str(j.value) + " ", end="")
                 else:
                     print("None ", end="")
             print("]")
 
+    def getDictRes(self): #Retorna un diccionario con los nombres de resistencias y sus nombres
+        result = {}
+        for i in self.adMatrix:
+            for j in i:
+                if j != None:
+                    temp = {j.component: j.value}
+                    result.update(temp)
+        return result
+
+    def getRes(self): #Retorna una lista con las resistencias desordenadas
+        result = []
+        for i in self.adMatrix:
+            for j in i:
+                if j != None:
+                    result+=[j.value]
+        return result
 
     def dijkstra(self, id1, id2, find):
         funct = None
         if find:
-            funct = lambda a,b: a>b
+            funct = lambda a, b: a > b
         else:
-            funct = lambda a,b: a<b
+            funct = lambda a, b: a < b
         queue = [id1]
         visted = []
         output = []
@@ -127,16 +143,16 @@ class Graph:
         final = self.getById(id2)
         finished = False
         chart = []
-        
+
         for i in range(self.nodesCount):
-            chart_i = [self.nodes[i].getId(),-1, ""]
+            chart_i = [self.nodes[i].getId(), -1, ""]
             chart += [chart_i]
-            
+
         for i in chart:
             print(i)
 
         while not finished:
-            
+
             if queue == []:
                 print("No hay camino hacia ese nodo")
                 finished = True
@@ -144,7 +160,7 @@ class Graph:
             current = queue[0]
             current_i = self.nodes.index(self.getById(current))
             queue = queue[1:]
-            #print(current)
+            # print(current)
             if current == initial.getId():
                 chart[current_i][1] = 0
             else:
@@ -156,8 +172,8 @@ class Graph:
                     if i[current_i] != None:
                         values += [i[current_i].getValue()]
                         preds += [self.adMatrix.index(i)]
-                #print(preds, values)
-                
+                # print(preds, values)
+
                 tmp_path = 0
                 for i in range(len(preds)):
                     if chart[preds[i]][1] != -1:
@@ -175,18 +191,17 @@ class Graph:
 
                 for i in chart:
                     print(i)
-                 
+
             for j in range(len(self.adMatrix[current_i])):
                 if self.adMatrix[current_i][j] != None and not self.nodes[j].getId() in queue:
                     queue += [self.nodes[j].getId()]
-
 
             if current == final.getId():
                 tmp = final.getId()
                 tmp_i = current_i
                 output += [final.getId()]
                 while tmp != initial.getId():
-                    #print(tmp)
+                    # print(tmp)
                     output += [chart[tmp_i][2]]
                     tmp = chart[tmp_i][2]
                     tmp_i = self.nodes.index(self.getById(tmp))
@@ -194,12 +209,12 @@ class Graph:
                 break
         twist_out = []
         for x in output:
-            twist_out = [x]+twist_out
+            twist_out = [x] + twist_out
         output = twist_out
         print(output)
 
+
 def radixSort(listIn):
-    
     n = 0
     biggest = 0
     for elem in listIn:
@@ -209,28 +224,28 @@ def radixSort(listIn):
     biggest = len(str(biggest))
     print(biggest)
     print(listIn)
-    
+
     for x in range(biggest):
         print("###########################")
         listOut = []
         for i in range(len(listIn)):
             listOut += [0]
-        digit = 10**x
+        digit = 10 ** x
         integers = []
         for i in range(10):
             integers += [0]
         for elem in listIn:
-            dig = (elem//digit)%10
-            #print(dig)
+            dig = (elem // digit) % 10
+            # print(dig)
             integers[dig] += 1
         for i in range(len(integers)):
             if i != 0:
-                integers[i] += integers[i-1]
-        #print(integers)
+                integers[i] += integers[i - 1]
+        # print(integers)
         print(listIn)
         for i in range(len(listIn)):
-            n = len(listIn) - (i+1)
-            dig = (listIn[n]//digit)%10
+            n = len(listIn) - (i + 1)
+            dig = (listIn[n] // digit) % 10
             integers[dig] -= 1
             num = listIn[n]
             listOut[integers[dig]] = num
@@ -238,16 +253,17 @@ def radixSort(listIn):
         listIn = listOut
     return listOut
 
+
 def insertionSort(listOrd):
     position = 1
     while position < len(listOrd):
         ordered = False
         i = position
         while not ordered:
-            if listOrd[i] > listOrd[i-1] and i != 0:
+            if listOrd[i] > listOrd[i - 1] and i != 0:
                 tmp = listOrd[i]
-                listOrd[i] = listOrd[i-1]
-                listOrd[i-1] = tmp
+                listOrd[i] = listOrd[i - 1]
+                listOrd[i - 1] = tmp
                 i -= 1
             else:
                 ordered = True
@@ -255,27 +271,36 @@ def insertionSort(listOrd):
 
     return listOrd
 
-def main():
+def searchNameRes(dictionary, list): #Busca el nombre de las resistencias tomando un diccionario con estas y una lista ordenada de las resistencias
+    result = {}
+    for i in list:
+        for j in dictionary:
+            if i == dictionary.get(j):
+                temp = {j:i}
+                result.update(temp)
+    return result
 
+def main():
+    """
     print(radixSort([170, 45, 75, 90, 802, 24, 2, 66]))
     print(insertionSort([170, 45, 75, 90, 802, 24, 2, 66]))
+"""
 
-    '''
     graph = Graph()
     graph.addNode("A")
     graph.addNode("B")
     graph.addNode("C")
     graph.addNode("D")
     graph.addNode("E")
-    graph.addArc("A", "B", "resistor", 10)
-    graph.addArc("B", "C", "resistor", 20)
-    graph.addArc("B", "D", "resistor", 40)
-    graph.addArc("C", "D", "resistor", 10)
-    graph.addArc("A", "C", "resistor", 40)
-    graph.addArc("C", "E", "resistor", 40)
-    graph.addArc("D", "A", "resistor", 20)
-    graph.dijkstra("D", "E", False)
-    '''
+    graph.addArc("A", "B", "R1", 10)
+    graph.addArc("B", "C", "R2", 20)
+    graph.addArc("B", "D", "R3", 40)
+    graph.addArc("C", "D", "R4", 10)
+    graph.addArc("A", "C", "R5", 40)
+    graph.addArc("C", "E", "R6", 40)
+    graph.addArc("D", "A", "R7", 20)
+    # graph.dijkstra("D", "E", False)
+    slist = insertionSort(graph.getRes())
+    searchNameRes(graph.getDictRes(),slist)
 
 main()
-
